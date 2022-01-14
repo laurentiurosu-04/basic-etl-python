9b8c078ca51c95dc7fac0df21a39d65059430fa9
laurentiu.rosu <50487193+LaurentiuRosu@users.noreply.github.com>
2022-01-14 15:07:07 +0200

    Feat: Transform part added for data
    With a function that verify data

--- main.py
+++ main.py
@@ -7,13 +7,40 @@ from datetime import datetime
 import datetime
 import sqlite3
 
-DATEBASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 USER_ID = ""
 TOKEN = ""
 
+
+def check_if_data_valid(df: pd.DataFrame) -> bool:
+    if df.empty:
+        print('No songs downloaded. Finishing execution')
+        return False
+
+    # primary key check
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception('Primary key is violated')
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+    # Check that all timestamps are of yesterday's date
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+            raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
+    return True
+
+
 if __name__ == '__main__':
     """
-    # Extract part of the ETL process from spotify API
+    Extract part of the ETL process from spotify API
     """
     headers = {
         "Accept": "application/json",
@@ -26,7 +53,9 @@ if __name__ == '__main__':
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
     # Download all songs you've listened to "after yesterday", which means in the last 24 hours
-    r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
+    r = requests.get(
+        "https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp),
+        headers=headers)
     data = r.json()
     print(data)
 
@@ -50,5 +79,12 @@ if __name__ == '__main__':
         "timestamp": timestamps
     }
 
+    # for x,y in song_dict.items():
+    #   print(x,y)
+
     song_df = pd.DataFrame(song_dict, columns=['song_name', 'artist_name', 'played_at', 'timestamp'])
-    print(song_df)
+    print(song_df.to_string())
+
+    # Validate
+    if check_if_data_valid(song_df):
+        print('Data valid, proceed to load stage')
